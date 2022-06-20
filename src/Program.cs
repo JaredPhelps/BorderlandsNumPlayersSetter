8c39a5dfbeb70935ac8a16a09bb3521b93a3846a
Jared Phelps <jared@inksoft.com>
2022-06-20 14:45:20 -0700

    Change to branch A

--- src/Program.cs
+++ src/Program.cs
@@ -15,6 +15,7 @@ namespace BorderlandsNumPlayersSetter
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            // change from branch A here
         }
     }
 }
