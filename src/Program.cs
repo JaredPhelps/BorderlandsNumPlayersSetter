2efdc90a6c3357f604541deac2b249f0b4783267
Jared Phelps <jared@inksoft.com>
2022-06-20 11:55:25 -0700

    Test Commit #2

--- src/Program.cs
+++ src/Program.cs
@@ -13,6 +13,7 @@ namespace BorderlandsNumPlayersSetter
         static void Main()
         {
             // Here is a change made on the test-github-pr-diff-source1 branch
+            // Here is a change made on the test-github-pr-diff-source2 branch, which was based on the source1 branch
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
